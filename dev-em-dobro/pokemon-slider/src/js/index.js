387e0a9e4014251691e9e038a7f88819169438ce
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-08-14 23:48:23 -0300

    removing comments

--- dev-em-dobro/pokemon-slider/src/js/index.js
+++ dev-em-dobro/pokemon-slider/src/js/index.js
@@ -1,19 +1,3 @@
-/*
-
-OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o próximo cartão da lista
-    - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
-    - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
-    - passo 3 - fazer aparecer o próximo cartão da lista 
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-
-OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão anterior da lista
-    - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
-    - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
-    - passo 3 - fazer aparecer o cartão anterior da lista 
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-
-*/
-
 const btnAvancar = document.getElementById('btnAvancar')
 const btnVoltar = document.getElementById('btnVoltar')
 const cartoes = document.querySelectorAll('.cartao')
