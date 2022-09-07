9373c07520aea85bb157084ddc58511fa98f8d11
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-09-07 19:51:28 -0300

    few fixes and a small progress

---
+++ frontend-mentor/intro-component-with-signup-form-master/src/js/index.js
@@ -0,0 +1,12 @@
+function formValidator() {
+  const firstName = document.getElementById('firstname');
+  const lastName = document.getElementById('lastname');
+  const email = document.getElementById('email');
+  const password = document.getElementById('firstname');
+  const input = document.querySelectorAll('input');
+ 
+  if (firstName.value == '') {
+
+  
+  }
+}
