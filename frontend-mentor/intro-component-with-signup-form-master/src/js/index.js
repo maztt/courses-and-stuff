fb979298cf9e3df4712b7d8cc06cc6d75a5eadc7
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-09-09 22:13:08 -0300

    minor fixes and attempts

--- frontend-mentor/intro-component-with-signup-form-master/src/js/index.js
+++ frontend-mentor/intro-component-with-signup-form-master/src/js/index.js
@@ -1,12 +1,20 @@
-function formValidator() {
-  const firstName = document.getElementById('firstname');
-  const lastName = document.getElementById('lastname');
-  const email = document.getElementById('email');
-  const password = document.getElementById('firstname');
-  const input = document.querySelectorAll('input');
- 
-  if (firstName.value == '') {
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const input = document.querySelectorAll('input');
 
-  
+function formValidator() {
+  if (firstName == '') {
+    firstName.validationMessage('abc');
+    firstName.setCustomValidity('dsa')
   }
 }
+
+// firstName.addEventListener("blur", () => {
+//   if (firstName.isEmpty()) {
+
+//   } else {
+
+//   }
+// })
