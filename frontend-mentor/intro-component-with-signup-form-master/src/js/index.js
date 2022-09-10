eed60671cc7194539bedb58041ce7c3e2ecfe0d8
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-09-10 10:59:53 -0300

    attempts

--- frontend-mentor/intro-component-with-signup-form-master/src/js/index.js
+++ frontend-mentor/intro-component-with-signup-form-master/src/js/index.js
@@ -5,11 +5,47 @@ const password = document.getElementById('password');
 const input = document.querySelectorAll('input');
 
 function formValidator() {
-  if (firstName == '') {
-    firstName.validationMessage('abc');
-    firstName.setCustomValidity('dsa')
+
+  const firstNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+
+  if (firstNameValue === '') {
+    setErrorFor(firstName, 'First Name cannot be blank');
+  } else {
+    setSuccessFor(firstName);
+  }
+
+  if (lastNameValue === '') {
+    setErrorFor(lastName, 'Last Name cannot be blank');
+  } else {
+    setSuccessFor(lastName);
+  }
+
+  if (emailValue === '') {
+    setErrorFor(email, 'First Name cannot be blank');
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === '') {
+    setErrorFor(password, 'Password cannot be blank');
+  } else {
+    setSuccessFor(password);
   }
 }
+
+function setErrorFor(input, message) {
+}
+
+function setSuccessFor(input) {
+}
+	
+// function isEmail(email) {
+// 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+// }
 
 // firstName.addEventListener("blur", () => {
 //   if (firstName.isEmpty()) {
