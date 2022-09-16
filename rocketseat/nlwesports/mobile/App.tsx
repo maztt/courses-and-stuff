4a21609e6e5af2cbd192223cd166054c5dee676b
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-09-16 13:44:03 -0300

    updating mobile progress - day 04

--- rocketseat/nlwesports/mobile/App.tsx
+++ rocketseat/nlwesports/mobile/App.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter'
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,7 +17,7 @@ export default function App() {
       <StatusBar barStyle="light-content"
       backgroundColor="transparent"
       translucent/>
-      { fontsLoaded ? <Home /> : <Loading /> }
+      { fontsLoaded ? <Routes /> : <Loading /> }
     </Background>
   );
 }
