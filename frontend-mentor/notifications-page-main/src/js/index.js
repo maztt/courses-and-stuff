e950537a967d0f007b8d196af872178a522d3c05
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-11-13 12:23:25 -0300

    fix: css improvements, query/html adjustments and js kickoff

---
+++ frontend-mentor/notifications-page-main/src/js/index.js
@@ -0,0 +1,19 @@
+const numNotifications = document.getElementById('number-notifications');
+const marker = document.getElementById('btn-read-all')
+
+// 1. Count how many content-boxes are within "messages unread"
+
+
+// 2. Get that count and display in numNotifications
+
+
+// 3. Clicking marker should change unread to read within the classes
+
+
+// 4. All red dots elements inside divs should be also removed
+
+
+
+marker.addEventListener('click', (e) => {
+  console.log('test')
+})
