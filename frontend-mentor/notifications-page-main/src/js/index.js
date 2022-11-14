a6ed368f0fd1d0a00d291af46466cd90160227d1
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-11-14 12:09:30 -0300

    feat: add js script (finished challenge)

--- frontend-mentor/notifications-page-main/src/js/index.js
+++ frontend-mentor/notifications-page-main/src/js/index.js
@@ -1,19 +1,38 @@
 const numNotifications = document.getElementById('number-notifications');
 const marker = document.getElementById('btn-read-all')
+const divs = document.querySelector('div.messages.unread')
+const dots = document.querySelectorAll('span.dot')
+
+let counter = 0;
 
 // 1. Count how many content-boxes are within "messages unread"
-
+for (const _child of divs.children) {
+  counter++;
+}
 
 // 2. Get that count and display in numNotifications
+numNotifications.innerText = counter;
 
 
 // 3. Clicking marker should change unread to read within the classes
-
-
 // 4. All red dots elements inside divs should be also removed
-
-
-
 marker.addEventListener('click', (e) => {
-  console.log('test')
+
+  for (const child of divs.children) {
+
+    if (counter > 0) {
+      counter--;
+    } else {
+      return;
+    }
+
+    child.parentElement.classList.remove('unread');
+    child.parentElement.classList.add('read')
+  }
+
+  dots.forEach((dot) => {
+    dot.innerText = '';
+  })
+  
+  numNotifications.innerText = counter;
 })
