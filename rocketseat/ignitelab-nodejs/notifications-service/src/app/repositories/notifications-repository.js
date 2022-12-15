6b786f6c9b810f51987cc37943aaba10e67b09a3
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-12-15 17:33:42 -0300

    feat: add routes, controllers, validators and others

--- rocketseat/ignitelab-nodejs/notifications-service/src/app/repositories/notifications-repository.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.NotificationsRepository = void 0;
-var NotificationsRepository = /** @class */ (function () {
-    function NotificationsRepository() {
-    }
-    return NotificationsRepository;
-}());
-exports.NotificationsRepository = NotificationsRepository;
