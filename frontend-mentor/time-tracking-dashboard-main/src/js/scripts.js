8d67d868f2e204517852c707da2f11dc4c455241
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2022-12-16 16:45:44 -0300

    styles: fix query and structure / feat: add script,

--- frontend-mentor/time-tracking-dashboard-main/src/js/scripts.js
+++ frontend-mentor/time-tracking-dashboard-main/src/js/scripts.js
@@ -1,1 +1,68 @@
-import { data } from '../data'
+fetch('src/data/data.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+
+const optDaily = document.getElementById('daily')
+const optWeekly = document.getElementById('weekly')
+const optMonthly = document.getElementById('monthly')
+const spans = document.getElementsByTagName('span')
+
+const dailyData = data.map(item => {
+  const { title, timeframes } = item;
+  const { daily } = timeframes;
+  return {
+    title,
+    daily
+  };
+});
+
+const weeklyData = data.map(item => {
+  const { title, timeframes } = item;
+  const { weekly } = timeframes;
+  return {
+    title,
+    weekly
+  };
+});
+
+const monthlyData = data.map(item => {
+  const { title, timeframes } = item;
+  const { monthly } = timeframes;
+  return {
+    title,
+    monthly
+  };
+  });
+
+
+const allCurrentData = []
+
+for (let i = 0; i < spans.length; i++) {
+  if (spans[i].classList.contains('current')) {
+    allCurrentData.push(spans[i])
+  }
+}
+
+
+
+
+optDaily.addEventListener('click', () => {
+  
+  dailyDataApply()
+})
+
+optWeekly.addEventListener('click', () => {
+  console.log('click')
+})
+
+optMonthly.addEventListener('click', () => {
+  console.log('click')
+})
+
+function dailyDataApply() {
+  allCurrentData.forEach((data) => {
+    data.innerHTML = data
+  })
+}
