9ed97b07cfd6bdb3ab6c3fb11197adcf529e5b09
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-02-14 10:15:31 -0300

    feat: add route for completing habits

--- rocketseat/nlw-setup/server/src/lib/routes.ts
+++ rocketseat/nlw-setup/server/src/lib/routes.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify'
-import { prisma } from "./lib/prisma"
+import { prisma } from "./prisma"
 import { z } from 'zod'
 import dayjs from 'dayjs'
 
@@ -75,6 +75,57 @@ export async function appRoutes(app: FastifyInstance) {
       possibleHabits,
       completedHabits,
     }
+  })
+
+  app.patch('/habits/:id/toggle', async (req, res) => {
+
+    const toggleHabitParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toggleHabitParams.parse(req.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id
+        }
+      }
+    })
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      })
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id
+        }
+      })
+    }
+
+
   })
 }
 
